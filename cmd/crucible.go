b683a19d77d895c12cee71a14283d47b1bc9a0ef
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-12-03 23:39:06 +0000

    execute playbook

--- cmd/crucible.go
+++ cmd/crucible.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-  "fmt"
   "github.com/mrwilson/crucible"
   "os"
 )
 
 func main() {
-  fmt.Println(os.Args[1:])
   crucible.RunPlaybook(os.Args[1:])
 }
 
--- execute.go
+++ execute.go
@@ -3,19 +3,26 @@ package crucible
 import (
   "fmt"
   "os/exec"
+  "strings"
 )
 
-const ansibleCommand string = "ansible-playbook"
+const (
+  ansibleCommand string = "ansible-playbook"
+  crucibleDir    string = "/home/mrwilson/.crucible"
+)
 
 func RunPlaybook(options []string) {
+
+  options[0] = strings.Join([]string{crucibleDir,options[0],"site.yml"}, "/")
 
   cmd := exec.Command(ansibleCommand, options...)
   cmd.Env = nil
   out, err := cmd.CombinedOutput()
 
   if err != nil {
-    fmt.Println("It's all gone wrong!")
+    fmt.Println(err)
   }
 
   fmt.Println(string(out))
+
 }
